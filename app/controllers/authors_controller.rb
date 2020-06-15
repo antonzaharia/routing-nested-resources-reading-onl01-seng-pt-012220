64e9fde8238bf78ed3151d07217a334f46e0fdef
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-15 18:07:10 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,4 +8,11 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
     @posts = @author.posts
     render template: 'posts/index'
+  end
+
+  def post
+    @author = Author.find(params[:id])
+    @post = Post.find(params[:post_id])
+    render template: 'post/show'
+  end
 end
